5e43a001960a83f0056e747fa99014331e360847
sydleither <sydleither@protonmail.com>
2024-02-22 14:59:03 -0500

    plots faceted

--- analysis.py
+++ analysis.py
@@ -54,26 +54,6 @@ def score_correlation(df, param_names, exp_name, grouping):
     plt.close()
 
 
-def score_heatmap(df, exp_name, constraints):
-    if len(constraints) != 2:
-        return
-    constraint0_targets = sorted(df[constraints[0]].unique())
-    constraint1_targets = sorted(df[constraints[1]].unique())
-    constraint_means = []
-    for target1 in constraint1_targets:
-        constraint_means_row = []
-        for target0 in constraint0_targets:
-            constraint_means_row.append(df.loc[(df[constraints[0]] == target0) & (df[constraints[1]] == target1)]["score"].mean())
-        constraint_means.append(constraint_means_row)
-    df_t = pd.DataFrame(constraint_means, index=constraint0_targets, columns=constraint1_targets)
-    figure = plt.figure()
-    ax = sns.heatmap(df_t, center=0, vmin=min(df["score"]), vmax=max(df["score"]), cmap="coolwarm")
-    ax.set(xlabel=constraints[0], ylabel=constraints[1], title="Mean Score")
-    figure.tight_layout()
-    plt.savefig(f"{get_plots_path()}{exp_name}/heatmap_mean_score.png")
-    plt.close()
-
-
 def score_heatmaps(df, exp_name, constraints):
     def build_heatmap(*args, **kwargs):
         df = kwargs.pop("data")
@@ -86,7 +66,7 @@ def score_heatmaps(df, exp_name, constraints):
                 constraint_means_row.append(df.loc[(df[constraints[0]] == target0) & (df[constraints[1]] == target1)]["score"].mean())
             constraint_means.append(constraint_means_row)
         df_t = pd.DataFrame(constraint_means, index=constraint0_targets, columns=constraint1_targets)
-        sns.heatmap(df_t, center=0, vmin=min(df["score"]), vmax=max(df["score"]), cmap="coolwarm")
+        sns.heatmap(df_t, center=0, vmin=min(df["score"]), vmax=max(df["score"]), cmap="PiYG", square=True)
 
     def create_heatmaps_facet(df, exp_name, figure, title=None, facet=""):
         figure.map_dataframe(build_heatmap, sorted(df[constraints[0]].unique()), sorted(df[constraints[1]].unique()))
@@ -94,7 +74,7 @@ def score_heatmaps(df, exp_name, constraints):
         figure.tight_layout()
         if title:
             figure.fig.suptitle(title)
-        figure.savefig(f"{get_plots_path()}{exp_name}/histogram_score_facet{facet}.png")
+        figure.savefig(f"{get_plots_path()}{exp_name}/heatmap_score_facet{facet}.png")
 
     if len(constraints) == 2:
         figure = sns.FacetGrid(df)
@@ -116,13 +96,22 @@ def param_correlation_heatmap(df, param_names, exp_name):
     figure = plt.figure()
     df_params = df[param_names]
     corr = df_params.corr()
-    sns.heatmap(corr, center=0, vmin=-1, vmax=1, cmap="coolwarm")
+    sns.heatmap(corr, center=0, vmin=-1, vmax=1, cmap="PiYG")
     figure.tight_layout()
     plt.savefig(f"{get_plots_path()}{exp_name}/correlation_param.png")
     plt.close()
 
 
-def plot_score_correlated_properties(df, param_names, exp_name):
+def plot_score_correlated_properties(df, param_names, exp_name, constraints):
+    def create_scatter_facet(df, param, exp_name, figure, title=None, facet=""): 
+        figure.map_dataframe(sns.scatterplot, x=param, y="score")
+        figure.set_axis_labels(param, "score")
+        figure.set(xlim=(min(df[param]), max(df[param])), ylim=(min(df["score"]), max(df["score"])))
+        figure.tight_layout()
+        if title:
+            figure.fig.suptitle(title)
+        figure.savefig(f"{get_plots_path()}{exp_name}/score_{param}_facet{facet}.png")
+
     configs = sorted(df["config_num"].unique())
     num_params = len(param_names)
     highly_correlated = set()
@@ -138,32 +127,17 @@ def plot_score_correlated_properties(df, param_names, exp_name):
                 highly_correlated.add(param)
 
     for p in highly_correlated:
-        max_p = max(df[p])
-        min_p = min(df[p])
-        max_score = max(df["score"])
-        min_score = min(df["score"])
-        num_configs = len(configs)
-        fig_col_cnt = 2 if num_configs <= 4 else 4
-        fig_row_cnt = int(np.ceil(num_configs/fig_col_cnt))
-        figure, axis = plt.subplots(fig_row_cnt, fig_col_cnt, figsize=(5*fig_row_cnt, 3*fig_col_cnt), squeeze=False)
-        fig_row = 0
-        fig_col = 0
-        for i in configs:
-            df_group = df.loc[df["config_num"] == i]
-            axis[fig_row][fig_col].scatter(df_group[p], df_group["score"])
-            axis[fig_row][fig_col].set_title(f"Config {i}, r = {round(correlations[i][p], 2)}")
-            axis[fig_row][fig_col].set_xlim(min_p, max_p)
-            axis[fig_row][fig_col].set_ylim(min_score, max_score)
-            fig_col += 1
-            if fig_col % fig_row_cnt == 0:
-                fig_row += 1
-                fig_col = 0
-        figure.tight_layout(rect=[0, 0.03, 1, 0.95])
-        figure.suptitle(f"{exp_name} scatter plots")
-        figure.supylabel("Score")
-        figure.supxlabel(p)
-        plt.savefig(f"{get_plots_path()}{exp_name}/score_{p}.png")
-        plt.close()
+        if len(constraints) == 1:
+            figure = sns.FacetGrid(df, col=constraints[0])
+            create_scatter_facet(df, p, exp_name, figure, title=None, facet="")
+        elif len(constraints) == 2:
+            figure = sns.FacetGrid(df, col=constraints[0], row=constraints[1])
+            create_scatter_facet(df, p, exp_name, figure, title=None, facet="")
+        elif len(constraints) == 3:
+            for i,facet_target in enumerate(df[constraints[2]].unique()):
+                df_facet = df.loc[df[constraints[2]] == facet_target]
+                figure = sns.FacetGrid(df_facet, col=constraints[0], row=constraints[1])
+                create_scatter_facet(df, p, exp_name, figure, title=f"{constraints[2]} = {facet_target}", facet=i)
 
 
 """
@@ -174,7 +148,6 @@ def read_data(exp_name):
     df = pd.read_pickle(f"{file_path}{exp_name}.pkl")
     param_names = list(set(df.columns) - set(get_non_property_column_names()))
     df = df.reset_index()
-    df["adaptive"] = np.where(df["score"] > 2, True, False)
 
     config_num = df["config_num"].unique()[0]
     config = json.load(open(f"{get_configs_path()}{exp_name}/{config_num}.json"))
@@ -185,7 +158,7 @@ def read_data(exp_name):
     return df, param_names, constraints
 
 
-def main(exp_name, config_level_analysis=False):
+def main(exp_name):
     df, param_names, constraints = read_data(exp_name)
 
     path = f"{get_plots_path()}{exp_name}"
@@ -193,7 +166,7 @@ def main(exp_name, config_level_analysis=False):
         os.makedirs(path)
 
     score_correlation(df, param_names, exp_name, "config_num")
-    plot_score_correlated_properties(df, param_names, exp_name)
+    plot_score_correlated_properties(df, param_names, exp_name, constraints)
     score_histograms(df, exp_name, constraints)
     score_heatmaps(df, exp_name, constraints)
 
